cc72ad86b5069501f12de87e1c52b30a7430d0e6
Semanur Orhan <semanurorhan24@gmail.com>
2024-02-17 22:49:58 +0300

    first.up

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState(" ");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://api.weatherapi.com/v1/forecast.json?key=${
+            import.meta.env.VITE_WEATHER_API
+          }&q=${location}&days=4&aqi=yes&alerts=yes`
+        );
+        console.log(response);
+        setWeatherData(response.data);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+      if (location) {
+        fetchData();
+      }
+    };
+  }, [location]);
+
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app-container">
+        <h1 className="app-title"> Hava Durumu Uygulaması </h1>
+        <div className="input-container">
+          <input
+            className="location-input"
+            type="text"
+            placeholder='Şehir giriniz'
+            value={location}
+            onChange={handleLocationChange}
+          />
+          {weatherData && (
+            
+
+            <div className='weather-container'>
+              {weatherData.forecast.forecastday.map()
+              }
+
+            </div>
+
+          )}<div>
+
+
+
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
