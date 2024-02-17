23fb58fa179a7e4abfd1efb4886daf061b91844c
Semanur Orhan <semanurorhan24@gmail.com>
2024-02-17 22:37:33 +0300

    first.up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState(" ");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://api.weatherapi.com/v1/forecast.json?key=${
+            import.meta.env.VITE_WEATHER_API
+          }&q=${location}&days=4&aqi=yes&alerts=yes`
+        );
+        console.log(response);
+        setWeatherData(response.data);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+      if (location) {
+        fetchData();
+      }
+    };
+  }, [location]);
+
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app-container">
+        <h1 className="app-title"> Hava Durumu Uygulaması </h1>
+        <div className="input-container">
+          <input
+            className="location-input"
+            type="text"
+            placeholder='Şehir giriniz'
+            value={location}
+            onChange={handleLocationChange}
+          />
+          {weatherData && (
+            
+
+            <div className='weather-container'>
+              {weatherData.forecast.forecastday.map()
+              }
+
+            </div>
+
+          )}<div>
+
+
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
